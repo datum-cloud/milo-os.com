852d214671f4ca476df6c5dfec148400fc4e895e
Ronggur Mangaraja Habibun <ronggur.mh@gmail.com>
2025-10-17 09:20:31 +0700

    feat: new about page

---
+++ src/types/block.ts
@@ -0,0 +1,12 @@
+/**
+ * Type definitions for block components
+ */
+
+export interface Block {
+  id?: string;
+  class?: string;
+  description?: string;
+  url?: string;
+  title?: string;
+  reverse?: boolean;
+}
